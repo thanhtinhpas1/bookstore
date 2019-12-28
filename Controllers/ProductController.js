854e89672245c6db5afa6a0111cea429212467b5
thanhtinhpas1 <thanhtinhpas1@gmail.com>
2019-12-28 10:47:39 +0700

    update

--- Controllers/ProductController.js
+++ Controllers/ProductController.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var ProductRePo = require('../repos/ProductRePo');
+var ProductRePo = require('../repos/productRePo');
 
 var app = express();
 var router = express.Router();
